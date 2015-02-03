3d72c7e91509dbf8b6a4de55e31905a789dbcc20
Kris Jenkins <krisajenkins@gmail.com>
2015-02-03 12:28:03 +0000

    Final code from the night.

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,11 @@
 module Main where
 
 import Graphics.Element (..)
-import Text (..)
+import Text (plainText)
+import List
 import Char (..)
-
--- innerDiamond : Char -> Char -> String
--- innerDiamond target char =
---   let prefixCount = target - char
---   in
+import String (..)
+import Debug
 
 prefix : Char -> Char -> Int
 prefix target current = (toCode target) - (toCode current)
@@ -15,7 +13,25 @@ prefix target current = (toCode target) - (toCode current)
 suffix : Char -> Int
 suffix current = (((toCode current) - (toCode 'A')) * 2) - 1
 
+singleLine : Char -> Char -> String
+singleLine target current =
+  let prefixString = repeat (prefix target current) " "
+      midString = if current == 'A'
+                    then ""
+                    else (repeat (suffix current) " ")
+                         ++ (fromChar current)
+  in prefixString ++ (fromChar current) ++ midString
+
+currents : Char -> List Char
+currents c =
+  let up = List.map fromCode [(toCode 'A')..(toCode c)]
+      down = (List.tail (List.reverse up))
+  in up ++ down
+
+diamondInner : Char -> List String
+diamondInner c = List.map (singleLine c) (currents c)
+
 diamond : Char -> String
-diamond char = "A"
+diamond char = join "\n" <| diamondInner char
 
 main = plainText "Hello"
--- test/Tests.elm
+++ test/Tests.elm
@@ -4,6 +4,8 @@ import Graphics.Element (Element)
 import ElmTest.Test (test, Test, suite, defaultTest)
 import ElmTest.Assertion (assert, assertEqual)
 import ElmTest.Runner.Element (runDisplay)
+import String (concat,fromChar)
+import List
 
 import Main (..)
 
@@ -11,14 +13,23 @@ tests : Test
 tests =
   suite "Test"
     [test "A" (assertEqual "A" (diamond 'A'))
-
-    -- ,test "B" (assertEqual " A\nB B\n A" (diamond 'B'))
+    ,test "B" (assertEqual " A\nB B\n A" (diamond 'B'))
 
     ,test "Prefix A->A" (assertEqual 0 (prefix 'A' 'A'))
     ,test "Prefix D->B" (assertEqual 2 (prefix 'D' 'B'))
 
     ,test "Suffix B" (assertEqual 1 (suffix 'B'))
     ,test "Suffix D" (assertEqual 5 (suffix 'D'))
+
+    ,test "Single Line A->A" (assertEqual "A" (singleLine 'A' 'A'))
+    ,test "Single Line B->A" (assertEqual " A" (singleLine 'B' 'A'))
+    ,test "Single Line B->B" (assertEqual "B B" (singleLine 'B' 'B'))
+    ,test "Single Line D->C" (assertEqual " C   C" (singleLine 'D' 'C'))
+
+    ,test "Currents A" (assertEqual "A" (concat <| List.map fromChar (currents 'A')))
+    ,test "Currents C" (assertEqual "ABCBA" (concat <| List.map fromChar (currents 'C')))
+
+    ,test "Diamond Inner A" (assertEqual ["A"] (diamondInner 'A'))
     ]
 
 main : Element
